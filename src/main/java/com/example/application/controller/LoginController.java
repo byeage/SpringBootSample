e6d63bdf595a501e5add689cd7749f5b5b7d8365
shenzhichao <shenzhichao@haishukeji.com>
2021-11-09 12:57:25 +0800

    layout

--- src/main/java/com/example/application/controller/LoginController.java
+++ src/main/java/com/example/application/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.example.application.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -10,6 +11,12 @@ public class LoginController {
     @GetMapping("/login")
     public  String getLogin() {
         return "login/login";
+    }
+
+
+    @PostMapping("/login")
+    public String postLogin() {
+        return "redirect:/user/list";
     }
 
 }
